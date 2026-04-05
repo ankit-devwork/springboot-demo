b1ae16a32de7edc9b6486e75d6fc89e413cdad3d
ankit-devwork <ankitsrivastava4u@gmail.com>
2026-04-04 23:33:20 -0400

    Implement DELETE functionality in BookController

--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api/books")
@@ -27,4 +28,12 @@ public class BookController {
         books.add(book);
     }
 
+    @DeleteMapping("/{bookName}")
+    public ResponseEntity<Void> deleteBook(@PathVariable String bookName) {
+        boolean isRemoved = books.remove(bookName);
+        if (!isRemoved) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
 }
